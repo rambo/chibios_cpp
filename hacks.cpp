08dea2513358ff5fb2b510d4127cb50630655470
Eero af Heurlin <rambo@iki.fi>
2014-09-07 03:07:57 +0300

    from https://raw.githubusercontent.com/fpoussin/ChibiOS-Examples/master/STM32F4-LCD-C++/hacks.cpp

---
+++ hacks.cpp
@@ -0,0 +1,20 @@
+
+/*
+ * The default pulls in 70K of garbage
+ */
+
+namespace __gnu_cxx {
+
+  void __verbose_terminate_handler() {
+    for(;;);
+  }
+}
+
+
+/*
+ * The default pulls in about 12K of garbage
+ */
+
+extern "C" void __cxa_pure_virtual() {
+  for(;;);
+}
