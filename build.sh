6420156f8cafb8b7c8edbfccccb7d9d9b1b66ac0
Eero af Heurlin <rambo@iki.fi>
2014-10-14 01:00:29 +0300

    fix the fatfs path (not that we used it yet or anything

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 git submodule init
 git submodule update
-#if [ ! -d ChibiOS/ext/fatfs ]
-#then
-#    CUR_PWD=`pwd`
-#    cd ChibiOS/ext/
-#    unzip fatfs-*.zip
-#    cd $CUR_PWD
-#fi
+if [ ! -d ChibiOS/os/ext/fatfs ]
+then
+    CUR_PWD=`pwd`
+    cd ChibiOS/os/ext/
+    unzip fatfs-*.zip
+    cd $CUR_PWD
+fi
 #grep -q 'swreset.patch applied' ChibiOS/os/ports/GCC/ARMCMx/crt0.c || patch ChibiOS/os/ports/GCC/ARMCMx/crt0.c swreset.patch
 
 make
